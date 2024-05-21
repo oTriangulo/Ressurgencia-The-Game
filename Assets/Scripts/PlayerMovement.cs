ad290b590490cfb25782b38fbd44934759f86ab5
oTriangulo <guusswb@gmail.com>
2024-05-21 19:47:57 -0300

    alguns comentários que faltavam
    criei alguns comentários no script de movimentação do jogador.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    [SerializeField] private float moveSpeed = 2f;
-    Vector2 movement;
+    public Rigidbody2D rb; // i called the rigidbody in the inspector to be able to use it with a rb variable
+    [SerializeField] private float moveSpeed = 2f; // i created a float variable to set the speed of the player
+    Vector2 movement; // i created a vector 2 variable to store the movement
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +15,15 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
+        // i setted the movement x and y to be the horizontal and vertical axis
     }
     
     private void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); // i moved the player using the rigidbody and the movement variable and the speed variable and the time fixed delta time (a method that uses the inside the game time)
     }
     
 }
